12f30a62c99b384c3b835e8b807f0d31acf295a5
TCMUGURLU <medet.ugurlu@atmosware.turkcell.com.tr>
2023-12-17 22:10:17 +0300

    secÄond commit

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -3,3 +3,5 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 B
 SymmetricTree.java,6\5\6578f5d283281e8be2d1c2fd5fac2250e2148b43
+=
+TreeNode.java,8\5\8579dd3941b3819066f7416c2c2e83c93cfa684c
--- SymmetricTree.java
+++ SymmetricTree.java
@@ -1,5 +1,13 @@
+import javax.swing.tree.TreeNode;
+
 public class SymmetricTree {
     public static void main(String[] args) {
 
     }
+
+    public static boolean isSymmetric(TreeNode treeNode){
+        return false;
+    }
+
+
 }
